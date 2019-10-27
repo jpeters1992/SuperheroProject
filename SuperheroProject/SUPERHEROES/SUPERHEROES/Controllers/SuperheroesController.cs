6efcdae99c15425aa1c8b195a7228aff906310ba
DESKTOP-CJ5TJR4\MASENAK <peter736@uwm.edu>
2019-10-26 21:32:47 -0500

    redid my repo - created my model, added Key attribute, created DbSet, setup my Db, created Controller, created views, built out the C in CRUD

---
+++ SuperheroProject/SUPERHEROES/SUPERHEROES/Controllers/SuperheroesController.cs
@@ -0,0 +1,99 @@
+﻿using SUPERHEROES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SUPERHEROES.Controllers
+{
+    public class SuperheroesController : Controller
+    {
+        ApplicationDbContext context;
+
+        //CONSTRUCTOR
+        public SuperheroesController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        // GET: Superheroes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Superheroes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Superheroes/Create
+        public ActionResult Create()
+        {
+            SuperHero newSuperHero = new SuperHero();
+            return View(newSuperHero);
+        }
+
+        // POST: Superheroes/Create
+        [HttpPost]
+        public ActionResult Create(SuperHero newSuperHero)
+        {
+            try
+            {
+                context.Superheroes.Add(newSuperHero);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superheroes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Superheroes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superheroes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Superheroes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
